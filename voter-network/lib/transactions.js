f8ca2898c1dee340266a01c046434c472dfdd624
MatthewShowman <matthew.t.showman@gmail.com>
2020-11-12 07:41:38 -0600

    Added the initial params for tranactions

---
+++ voter-network/lib/transactions.js
@@ -0,0 +1,156 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* global getAssetRegistry getFactory emit */
+
+
+/* ****************** PARTICIPANTS  ****************** */
+
+// VOTER
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.addVoter} addVoter The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.updateVoter} updateVoter The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.deleteVoter} deleteVoter The sample transaction instance.
+ * @transaction
+ */
+
+//VERIFIER
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.Verifier} addVerifier The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.updateVerifier} updateVerifier The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.deleteVerifier} deleteVerifier The sample transaction instance.
+ * @transaction
+ */
+
+
+
+
+/* ****************** ASSETS  ****************** */
+
+//ELECTION
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.addElection} addElection The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.updateElection} updateElection The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.deleteElection} deleteElection The sample transaction instance.
+ * @transaction
+ */
+
+//RACE
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.addRace} addRace The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.updateRace} updateRace The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.deleteRace} deleteRace The sample transaction instance.
+ * @transaction
+ */
+
+// CANDIDATE
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.addCandidate} addCandidate The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.updateCandidate} updateCandidate The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.deleteCandidate} deleteCandidate The sample transaction instance.
+ * @transaction
+ */
+
+// MEASURE
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.addMeasure} addMeasure The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.updateMeasure} updateMeasure The sample transaction instance.
+ * @transaction
+ */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.deleteMeasure} deleteMeasure The sample transaction instance.
+ * @transaction
+ */
+
+
+
+
+/* ****************** TRANSACTIONS  ****************** */
+
+// VOTING
+
+/**
+ * Sample transaction processor function.
+ * @param {org.univote.submitVote} submitVote The sample transaction instance.
+ * @transaction
+ */
