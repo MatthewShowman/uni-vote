fb36890b8281bde71f7914fe50633adce5fba4d5
ssfrolic <ssfrolic@uark.edu>
2020-11-28 00:37:21 -0600

    Added voter form and contest editing

---
+++ js/editContest.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+
+
+    //Get Contest var
+    $.urlParam = function (name) {
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+            .exec(window.location.search);
+
+        return (results !== null) ? results[1] || 0 : false;
+    }
+    var contestId = $.urlParam('contestId');
+    var currentElectionId;
+    var contestData;
+
+    //Populate Election dropdown
+    $.getJSON('http://localhost:3000/api/org.univote.Election', function (electionData) {
+        for (var i = 0; i < electionData.length; i++) {
+            $('#election').append('<option value ="' + electionData[i].electionId + '">' + electionData[i].electionName + '</option>')
+        };
+    });
+
+
+
+    //Populate boxes with current info and get current election id
+    $.getJSON('http://localhost:3000/api/org.univote.Contest/' + contestId, function (data) {
+        console.log(data);
+        contestData = data;
+        $("#contestName").val(data.contestName);
+        $("#contestDescription").val(data.contestDescription);
+        $("#jurisdiction").val(data.jurisdiction);
+        $("#election").val(data.election.split('#').pop());
+        currentElectionId = data.election.split('#').pop();
+        console.log(data.election);
+    });
+
+
+
+
+
+    //Submit Function
+    $("#submitBtn").click(function () {
+
+        //Put form responses in JSON 
+/*         contestRequest = {
+            "contestName": $("#contestName").val(),
+            "contestDescription": $("#contestDescription").val(),
+            "jurisdiction": $("#jurisdiction").val(),
+            "election": $("#election").val()
+        }; */
+        contestData.contestName = $("#contestName").val();
+        contestData.contestDescription = $("#contestDescription").val();
+        contestData.jurisdiction = $("#jurisdiction").val();
+        contestData.election = $("#election").val();
+        var request = {
+            "oldElection" : currentElectionId,
+            "newElection" : $("#election").val(),
+            "contest"     : contestId,
+            "contestName": $("#contestName").val(),
+            "contestDescription" :  $("#contestDescription").val(),
+            "jurisdiction" : $("#jurisdiction").val(),
+            "candidates" : contestData.candidates
+        }
+
+        console.log(request);
+
+        //PUT request to REST API - Contest
+        $.ajax({
+            url: "http://localhost:3000/api/org.univote.updateContest",
+            type: "POST",
+            dataType: "json",
+            data: request,
+            success: function (contest) {
+                var param = $.param(contest);
+                console.log(param);
+                if (confirm("Contest was successfully updated!"))
+                    document.location = 'Contests.html';
+            },
+            error: function (xhr, status) {
+                console.log(status);
+                console.log(xhr);
+                alert("There was a problem.");
+            }
+        });
+
+    });//End Submit Function
+
+    function removeContestFromElection(electionData) {
+        var index = electionData.contests.indexOf("resource:org.univote.Contest#" + contestId);
+        if (index > -1) {
+            electionData.contests.splice(index, 1);
+        }
+    };
+
+});
+
+
+
---
+++ js/voterForm.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    //Submit Function
+    $("#submitBtn").click(function () {
+      //Put form responses in JSON 
+      request = {
+        "firstName": $("#firstName").val(),
+        "lastName": $("#lastName").val(),
+        "address": $("#address").val(),
+        "city": $("#city").val(),
+        "state": $("#state").val(),
+        "zip": $("#zip").val(),
+        "jurisdiction": $("#jurisdiction").val()
+      };
+  
+      //POST request to REST API
+      $.ajax({
+        url: "http://localhost:3000/api/org.univote.addVoter",
+        type: "POST",
+        dataType: "json",
+        data: request,
+        success: function (voter) {
+          var param = $.param(voter);
+          console.log(param);
+          if (confirm("Voter was successfully created!"))
+            document.location = 'Voters.html';
+        },
+        error: function (xhr, status) {
+          console.log(status);
+          console.log(xhr);
+          alert("There was a problem.");
+        }
+      })
+  
+    });//End Submit Function
+
+});
